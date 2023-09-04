074f8e8035f47a51fb8a985917bd4fe82ec88f18
Todd Stellanova <tstellanova@users-noreply.github.com>
2023-09-04 11:04:56 -0700

    add text insets

--- src/main.rs
+++ src/main.rs
@@ -51,10 +51,10 @@ const DISPLAY_W: usize = 320;
 const DISPLAY_H: usize = 240;
 
 const DISPLAY_SIZE: Size = Size::new(DISPLAY_W as u32, DISPLAY_H as u32);
-const OUTPUT_VIEW_SIZE: Size = Size::new(DISPLAY_SIZE.width, (DISPLAY_SIZE.height*2)/3);
-const INPUT_VIEW_SIZE: Size = Size::new(DISPLAY_SIZE.width, DISPLAY_SIZE.height/3);
-const OUTPUT_VIEW_TOP_LEFT: Point = Point::new(0,0);
-const INPUT_VIEW_TOP_LEFT: Point = Point::new(0, OUTPUT_VIEW_SIZE.height as i32);
+const OUT_VIEW_SIZE: Size = Size::new(DISPLAY_SIZE.width, (DISPLAY_SIZE.height*2)/3);
+const IN_VIEW_SIZE: Size = Size::new(DISPLAY_SIZE.width, DISPLAY_SIZE.height/3);
+const OUT_VIEW_TOP_LEFT: Point = Point::new(0, 0);
+const IN_VIEW_TOP_LEFT: Point = Point::new(0, OUT_VIEW_SIZE.height as i32);
 
 // i2c bus address of T-Deck "T-Keyboard" (run by a separate microcontroller -- ESP32-C3)
 const LILYGO_KB_I2C_ADDRESS: u8 =     0x55;
@@ -65,10 +65,14 @@ const OUT_CHAR_W:u32 = OUTPUT_TEXT_FONT.character_size.width as u32;
 const OUT_CHAR_H:u32 = OUTPUT_TEXT_FONT.character_size.height as u32;
 const IN_CHAR_W:u32 = INPUT_TEXT_FONT.character_size.width as u32;
 const IN_CHAR_H:u32 = INPUT_TEXT_FONT.character_size.height as u32;
-const MAX_OUT_X_IDX: u32 = OUTPUT_VIEW_SIZE.width / OUT_CHAR_W; //expect 35?
-const MAX_OUT_Y_IDX:u32 = OUTPUT_VIEW_SIZE.height / OUT_CHAR_H; // expect 10?
-const MAX_IN_X_IDX:u32 = INPUT_VIEW_SIZE.width / IN_CHAR_W; //expect 35?
-const MAX_IN_Y_IDX:u32 = INPUT_VIEW_SIZE.height / IN_CHAR_H; // expect 2?
+const OUT_VIEW_TXT_TOP_LEFT: Point = Point::new(
+    OUT_VIEW_TOP_LEFT.x + OUT_CHAR_W as i32,  OUT_VIEW_TOP_LEFT.y + OUT_CHAR_H as i32);
+const IN_VIEW_TXT_TOP_LEFT: Point = Point::new(
+    IN_VIEW_TOP_LEFT.x + IN_CHAR_W  as i32,  IN_VIEW_TOP_LEFT.y + IN_CHAR_H as i32);
+const MAX_OUT_X_IDX: u32 = (OUT_VIEW_SIZE.width / OUT_CHAR_W) -1; //expect 35?
+const MAX_OUT_Y_IDX:u32 = (OUT_VIEW_SIZE.height / OUT_CHAR_H)-1; // expect 10?
+const MAX_IN_X_IDX:u32 = (IN_VIEW_SIZE.width / IN_CHAR_W) -1; //expect 35?
+const MAX_IN_Y_IDX:u32 = (IN_VIEW_SIZE.height / IN_CHAR_H)-1; // expect 2?
 
 // Rules for how to move the cursor position based on a single character input
 fn update_cursor_position( x_idx: &mut u32, y_idx: &mut u32, max_x_idx: u32, max_y_idx: u32, eol: bool,) {
@@ -125,36 +129,30 @@ fn main() -> ! {
     let mut board_periph_pin = io.pins.gpio10.into_push_pull_output();
     board_periph_pin.set_high().unwrap();
 
-    /*
-    #define BOARD_I2C_SDA       18
-#define BOARD_I2C_SCL       8
-     */
     // Setup 100 kHz i2c0 bus
     let mut i2c0_bus = I2C::new(
         perphs.I2C0,
-        io.pins.gpio18,
-        io.pins.gpio8,
+        io.pins.gpio18, //I2C0 SDA
+        io.pins.gpio8, //I2C0 SCL
         100u32.kHz(),
         &mut system.peripheral_clock_control,
         &clocks,
     );
 
-    // bus_i2c.write_read(LILYGO_KB_I2C_ADDRESS, &[0xaa], &mut kb_data).ok();
 
-
-    let tdeck_sclk = io.pins.gpio40;
-    let tdeck_miso = io.pins.gpio38;
-    let tdeck_mosi = io.pins.gpio41;
+    // let tdeck_sclk = io.pins.gpio40;
+    // let tdeck_miso = io.pins.gpio38;
+    // let tdeck_mosi = io.pins.gpio41;
     let tdeck_tft_cs = io.pins.gpio12.into_push_pull_output();
     let tdeck_tft_dc = io.pins.gpio11.into_push_pull_output();
     let mut tft_enable_pin =  io.pins.gpio42.into_push_pull_output();
-    let _tdeck_sdcard_cs = io.pins.gpio39;
+    // let _tdeck_sdcard_cs = io.pins.gpio39;
 
     let spi2_bus = Spi::new(
         perphs.SPI2,
-        tdeck_sclk,
-        tdeck_mosi,
-        tdeck_miso,
+        io.pins.gpio40, //tdeck_sclk,
+        io.pins.gpio41, //tdeck_mosi,
+        io.pins.gpio38, //tdeck_miso,
         tdeck_tft_cs,
         60u32.MHz(),
         SpiMode::Mode0,
@@ -192,7 +190,7 @@ fn main() -> ! {
         .stroke_width(1)
         .fill_color(Rgb565::BLACK)
         .build();
-    Rectangle::new(OUTPUT_VIEW_TOP_LEFT,OUTPUT_VIEW_SIZE)
+    Rectangle::new(OUT_VIEW_TOP_LEFT, OUT_VIEW_SIZE)
         .into_styled(output_view_box_style)
         .draw(&mut display).unwrap();
     // Draw a box around the input view
@@ -201,14 +199,10 @@ fn main() -> ! {
         .stroke_width(1)
         .fill_color(Rgb565::BLACK)
         .build();
-    Rectangle::new(INPUT_VIEW_TOP_LEFT,INPUT_VIEW_SIZE)
+    Rectangle::new(IN_VIEW_TOP_LEFT, IN_VIEW_SIZE)
         .into_styled(input_view_box_style)
         .draw(&mut display).unwrap();
 
-    // println!("start text render");
-
-
-    println!("max_x {} max_y {}", MAX_OUT_X_IDX, MAX_OUT_Y_IDX);
 
     // let text_style = MonoTextStyle::new(&text_font, Rgb565::RED);// no bg fill
     let output_text_style = MonoTextStyleBuilder::new()
@@ -224,7 +218,7 @@ fn main() -> ! {
         .build();
 
 
-    let text = "12345678901234567890123456789012345\r\n";
+    // let text = "12345678901234567890123456789012345\r\n";
     let mut output_x_idx: u32 = 0;
     let mut output_y_idx: u32 = 0;
     let mut input_x_idx: u32 = 0;
@@ -242,7 +236,7 @@ fn main() -> ! {
         io.pins.gpio44.into_floating_input(), //GPIO 44 U0RXD
         );
 
-    let mut serial_port = Uart::new_with_config(
+    let mut uart0 = Uart::new_with_config(
         perphs.UART0,
         Some(uart_config),
         Some(uart_pins),
@@ -250,31 +244,28 @@ fn main() -> ! {
         &mut system.peripheral_clock_control,
         );
 
-    // let _ = serial_port.write_str(&text);
-    // let _ = serial_port.flush();
-
     timer0.start(100u64.millis());
     loop {
         let mut rbuf:[u8;1] = [0u8];
 
-        // read from the serial port and display
+        // read from the serial port UART0 and display
         {
             // let mut read_count = 0;
-            if let Ok(rb) = serial_port.read() {
+            if let Ok(rb) = uart0.read() {
                 let eol = ( rb == 0x0d);
                 rbuf[0] = rb;
                 // TODO disable echo -- used for debugging purposes only
-                let _ = serial_port.write_bytes(&rbuf);
+                let _ = uart0.write_bytes(&rbuf);
                 if eol {
                     //tack on a linefeed
                     rbuf[0] = 0x0a;
-                    let _ = serial_port.write_bytes(&rbuf);
+                    let _ = uart0.write_bytes(&rbuf);
                 }
 
                 let _ = Text::new(core::str::from_utf8(&rbuf).unwrap(),
                                   Point::new(
-                                      OUTPUT_VIEW_TOP_LEFT.x + (output_x_idx * OUT_CHAR_W) as i32,
-                                      OUTPUT_VIEW_TOP_LEFT.y + (output_y_idx * OUT_CHAR_H) as i32),
+                                      OUT_VIEW_TXT_TOP_LEFT.x + (output_x_idx * OUT_CHAR_W) as i32,
+                                      OUT_VIEW_TXT_TOP_LEFT.y + (output_y_idx * OUT_CHAR_H) as i32),
                                   output_text_style)
                     .draw(&mut display)
                     .unwrap();
@@ -293,17 +284,17 @@ fn main() -> ! {
                         let eol = rbuf[0] == 0x0d;
                         let _ = Text::new(core::str::from_utf8(&rbuf).unwrap(),
                                           Point::new(
-                                              INPUT_VIEW_TOP_LEFT.x + (input_x_idx * OUT_CHAR_W) as i32,
-                                              INPUT_VIEW_TOP_LEFT.y + (input_y_idx * OUT_CHAR_H) as i32),
+                                              IN_VIEW_TXT_TOP_LEFT.x + (input_x_idx * OUT_CHAR_W) as i32,
+                                              IN_VIEW_TXT_TOP_LEFT.y + (input_y_idx * OUT_CHAR_H) as i32),
                                           kb_text_style)
                             .draw(&mut display)
                             .unwrap();
                         update_cursor_position(&mut input_x_idx, &mut input_y_idx, MAX_IN_X_IDX, MAX_IN_Y_IDX, eol);
-                        let _ = serial_port.write_bytes(&rbuf);
+                        let _ = uart0.write_bytes(&rbuf);
                         if eol {
                             //tack on a linefeed
                             rbuf[0] = 0x0a;
-                            let _ = serial_port.write_bytes(&rbuf);
+                            let _ = uart0.write_bytes(&rbuf);
                         }
                     }
                 },
